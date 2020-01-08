133d4f82c2da38cf460bf6827fe7987b50c4a435
Marco <47861439+Marcark@users.noreply.github.com>
2020-01-08 13:36:31 +0100

    distance-maps.php
    // return distance between 2 point of google maps.
    // return distance in meters or in km (if $u is!="m")
    // the coords will be pass in google maps format (decimal degrees)

---
+++ distance-maps.php
@@ -0,0 +1,23 @@
+<?php
+
+// return distance between 2 point of google maps.
+// return distance in meters or in km (if $u is!="m")
+// the coords will be pass in google maps format (decimal degrees)
+function dist($lat1,$lon1,$lat2,$lon2,$u) {
+
+$lat1 = $lat1 * 3.14159265358979323846 /180;
+$lat2 = $lat2 * 3.14159265358979323846 /180;
+$lon1 = $lon1 * 3.14159265358979323846 /180;
+$lon2 = $lon2 * 3.14159265358979323846 /180;
+
+$dist = 6372.795477598 * acos(sin($lat1) * sin($lat2) + cos($lat1) * cos($lat2) * cos($lon1-$lon2));        
+
+	if ($u=="m") {
+		$dist = $dist * 1000;
+	}
+	
+return round($dist,3);
+	
+}
+
+?>
